b8ddecc22837502c7d0fabfb20b79e8caf1958bf
Gabriel Richards <gabrieljrich@pm.me>
2024-08-29 09:38:36 -0500

    Finalize utils module

--- utils_gjrich.py
+++ utils_gjrich.py
@@ -10,22 +10,26 @@ Process:
 Code is not written from top to bottom; instead, it is written from the outside, in.
 Here's what a first draft of utils_case.py might look like:
 
-1. I start with this docstring at the very beginning.
-   I use it to clarify the purpose of my Python file and organize my thoughts.
+1. This docstring is included at the beginning to clarify the purpose of the Python file and organize it.
    
-2. I'll declare a global variable for my byline string and just set it to some simple text.
+2. A global variable is declared for the byline string and it is set it to some simple text.
 
-3. I'll declare a main() function for my module. When I run this script, I can use main() to test my byline.
+3. The main() function is defined for the module. When the script is run, main() can be used to test the byline.
 
-4. I'll add the boilerplate conditional execution code so I only run the main() function when 
-   this script is executed directly (but not when I import it into another file).
+4. Boilerplate conditional execution code is added so the main() function only runs when 
+   this script is executed directly (but not when imported into another file).
 
 Testing was completed in an online interpreter (https://www.online-python.com/)
+
+For the sake of fun, added variables were kooky. 
+This also could be used as a marketing scheme to convince potential clients to engage our services,
+since in the modern era quirkyness equates to intelligence & competence.
 '''
 
 
-''' In the fourth interation, basic statistics are introduced using Python. 
-These include min(), max(), and the statistics module.
+''' 5th and Final iteration: 
+   - added variables missed in iteration 3 (success_status, flight count, former clients, best numbers)
+   - Updated byline to include output from them
 '''
 
 
@@ -60,6 +64,18 @@ skills_offered: list = ["Data Analysis", "Machine Learning", "Harmonic Balancing
 # List of floats representing individual client satisfaction scores
 client_satisfaction_scores: list = [4.5, 4.4, 4.7, 4.2, 4.9, 5.0, 4.4, 4.4, 3.9, 4.7, 4.8]
 
+# Bool Custom Example - success status
+success_status: bool = True
+
+# Number of flights taken to meet clients
+flight_count: int = 410
+
+# List of strings representing companies we've worked with
+# Had some fun making up tangential tech company names
+former_clients: list = ["Pear", "Soothsayer", "Nile", "Masktome", "Webtoons"]
+
+# List of ints indicating the best numbers - no surprise that they are mostly primes
+best_numbers: list = [3, 7, 17, 37, 49, 51]
 
 
 ####################################################
@@ -70,13 +86,19 @@ client_satisfaction_scores: list = [4.5, 4.4, 4.7, 4.2, 4.9, 5.0, 4.4, 4.4, 3.9,
 
 
 # Basic Stats using built-in functions min(), max() and statistics module functions mean() and stdev()
-min_score: float = min(client_satisfaction_scores)
-max_score: float = max(client_satisfaction_scores)
-mean_score: float = statistics.mean(client_satisfaction_scores)
-stdev_score: float = statistics.stdev(client_satisfaction_scores)
+
+# calculated for both Client Satisfaction Scores and Best Numbers
+min_scores: float = min(client_satisfaction_scores)
+max_scores: float = max(client_satisfaction_scores)
+mean_scores: float = statistics.mean(client_satisfaction_scores)
+stdev_scores: float = statistics.stdev(client_satisfaction_scores)
 
 
 
+min_best_numbers: float = min(best_numbers)
+max_best_numbers: float = max(best_numbers)
+mean_best_numbers: float = statistics.mean(best_numbers)
+stdev_best_numbers: float = statistics.stdev(best_numbers)
 
 
 ####################################################
@@ -87,11 +109,35 @@ stdev_score: float = statistics.stdev(client_satisfaction_scores)
 byline: str = f"""
 ----------------------------------------------------
 Wheyland Pythoni: Building Better Modules
+
+A new and unique company that can serve 
+your specific needs - whatever they are.
+
+We can provide whatever tools your company thinks you need
+in exchange for money. Let's work together!
 ----------------------------------------------------
-Has International Clients:   {has_international_clients}
-Years in Operation:          {years_in_operation}
-Skills Offered:              {skills_offered}
-Client Satisfaction Scores:  {client_satisfaction_scores}
+Has International Clients:      {has_international_clients}
+Years in Operation:             {years_in_operation}
+Flights Taken to Clients:       {flight_count}
+Skills Offered:                 {skills_offered}
+Are we successful?              {success_status}
+Former Clients:                 {former_clients}
+
+Best Numbers:                   {best_numbers}
+Minimum Best Number:            {min_best_numbers}
+Maximum Best Number:            {max_best_numbers}
+Mean Best Number:               {mean_best_numbers:.3f}
+Best Number Standard Deviation: {stdev_best_numbers:.3f}
+
+Client Satisfaction Scores:     {client_satisfaction_scores}
+Minimum Score:                  {min_scores}
+Maximum Score:                  {max_scores}
+Mean Score:                     {mean_scores:.3f}
+Score Standard Deviation:       {stdev_scores:.3f}
+
+
+Please hire us! Or else!
+
 """
 
 
